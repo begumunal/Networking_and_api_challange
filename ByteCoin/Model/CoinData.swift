6f65252adb7f65050d6afd4254500ac646fabfb1
Begüm Ünal <77240132+begumunal@users.noreply.github.com>
2023-03-24 12:51:15 +0300

    convertCoin

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Begum Unal on 24.03.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData : Codable{
+    let rate : Double
+    
+}
